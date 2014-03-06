d80785e8b9dfc7c44ac96610539bb794db60fcc3
Joshwa Fugett <jfugett27@gmail.com>
2014-03-06 15:46:04 -0500

    Finished setting up the jshint watch tasks

--- grunt_config/watch.js
+++ grunt_config/watch.js
@@ -8,5 +8,17 @@ module.exports = {
 	jsApp: {
 		files: ['app/**/*.js'],
 		tasks: ['newer:jshint:app']
+	},
+	jsClient: {
+		files: ['client/**/*.js'],
+		tasks: ['newer:jshint:client']
+	},
+	jsTests: {
+		files: ['tests/**/*.js'],
+		tasks: ['newer:jshint:tests']
+	},
+	jsWorkers: {
+		files: ['workers/**/*.js'],
+		tasks: ['newer:jshint:workers']
 	}
 };

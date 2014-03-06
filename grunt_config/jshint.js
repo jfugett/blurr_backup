dd6bcf10b3fe238f73062b8f3bd933234da1a74d
Joshwa Fugett <jfugett27@gmail.com>
2014-03-05 21:20:56 -0500

    Finalized base jshint options - only need to integrate the watches now

--- grunt_config/jshint.js
+++ grunt_config/jshint.js
@@ -12,6 +12,24 @@ module.exports = {
             jshintrc: '.jshintrc'
         },
         src: ['/app/**/*.js']
-	
+
+	},
+	'client': {
+		options: {
+			jshintrc: '.jshintrc'
+		},
+		src: ['client/**/*.js']
+	},
+	'tests': {
+		options: {
+			jshintrc: '.jshintrc'
+		},
+		src: ['tests/**/*.js']
+	},
+	'workers': {
+		options: {
+			jshintrc: '.jshintrc'
+		},
+		src: ['workers/**/*.js']
 	}
 };

68f3a8d29517c0c78ed714260e9e04f6071c2c0b
Joshwa Fugett <jfugett27@gmail.com>
2014-03-06 12:33:27 -0500

    Added overall success and failure notifications

--- grunt_config/notify.js
+++ grunt_config/notify.js
@@ -6,5 +6,17 @@ module.exports = {
 			title: 'JSHint Finished',
 			message: 'JSHint has finished checking your javascript files'
 		}
+	},
+	success: {
+		options: {
+			title: 'Woot',
+			message: 'Completed Successfully',
+		}
+	},
+	failure: {
+		options: {
+			title: 'Uh Oh',
+			message: 'Something Went Wrong',
+		}
 	}
 };
